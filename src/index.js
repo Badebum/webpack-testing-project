d6378e3994527540a6475fd79d16c07a293153ed
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-11 16:57:21 +0300

    handlesbar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import './styles/index.scss'
-import './js/main.js'
+import './styles/index.scss';
+import './js/localstorage.js';
+import './js/main.js';
--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,7 @@
-import imageItems from './gallery-items.js';
+import cardsTemplate from '../templates/card.hbs';
+import cards from './menu.json';
 
-const galleryRef = document.querySelector('.js-gallery');
-const lightboxRef = document.querySelector('.js-lightbox');
-const clouseBtn = document.querySelector('[data-action="close-lightbox"]');
-const originImgRef = document.querySelector('.lightbox__image');
+const markup = cardsTemplate(cards);
+const listMenu = document.querySelector('.js-menu');
 
-imageItems.forEach((image, index) => {
-  const imageList = `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" data-index="${index}" alt="${image.description}"/></a></li>`;
-  galleryRef.insertAdjacentHTML('beforeend', imageList);
-});
-
-function openModal(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') return;
-  originImgRef.setAttribute('src', e.target.dataset.source);
-  originImgRef.setAttribute('alt', e.target.alt);
-  lightboxRef.classList.add('is-open');
-}
-
-function closeModal() {
-  lightboxRef.classList.remove('is-open');
-  originImgRef.setAttribute('src', '');
-  originImgRef.setAttribute('alt', '');
-}
-
-function btnClose(e) {
-  if (e.target === clouseBtn) {
-    closeModal();
-  }
-}
-
-function lightBoxClose(e) {
-  if (e.currentTarget.nodeName === e.target.nodeName) {
-    closeModal();
-  }
-}
-
-function closeEsc(e) {
-  if (e.code === 'Escape') {
-    closeModal();
-  }
-}
-
-function btnSlider(event) {
-  let index = event.target.firstElementChild.dataset.index;
-  if (event.code === 'ArrowLeft' && index > 0) {
-    index--;
-  }
-  if (event.code === 'ArrowRight' && index < imageItems.length - 1) {
-    index++;
-  }
-  event.target.firstElementChild.dataset.index = index;
-  originImgRef.setAttribute('src', imageItems[index].original);
-  originImgRef.setAttribute('alt', imageItems[index].description);
-}
-
-galleryRef.addEventListener('click', openModal);
-lightboxRef.addEventListener('click', btnClose);
-lightboxRef.addEventListener('click', lightBoxClose);
-window.addEventListener('keydown', closeEsc);
-window.addEventListener('keydown', btnSlider);
+listMenu.insertAdjacentHTML('afterbegin', markup);
