5bac0b5730518ed33add6f4a52b23b7b80ce39e6
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-16 16:11:39 +0300

    countrie

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
-import fetchContries from './js/fetchData.js';
-import countries from './templates/countries.hbs';
+import './styles.css';
+import API from './js/api/fetchCountries';
+import linksRef from './js/components/refs';
+// import _debounce from 'lodash';
 
-const refs = {
-  areaInput: document.querySelector('#areaInput'),
-  card: document.querySelector('.card-container'),
-};
+import { pnotifyError, closeNotice } from './js/components/notify';
+import createBaseMarkup from '../src/js/markup/createBaseMarkup';
+import createMarkup from '../src/js/markup/createMarkup';
+import countryCard from '../src/templates/countryCard.hbs';
+import countriesList from '../src/templates/countriesList.hbs';
 
-refs.areaInput.addEventListener('change', onChange);
+const _debounce = require('lodash.debounce');
 
-function onChange(e) {
-  e.preventDefault();
-  const country = refs.areaInput.value;
+createBaseMarkup();
 
-  fetchContries(country).then(cardCountriesRender);
+const { searchResultRef, inputRef } = linksRef();
+
+function inputChange({ target }) {
+  closeNotice();
+
+  const searchQuery = target.value;
+
+  if (!searchQuery.trim()) return;
+
+  API.fetchCountries(searchQuery)
+    .then(response => {
+      if (response.length === 1) {
+        const resultOneCountry = response.reduce((acc, item) => item);
+
+        createMarkup(searchResultRef, countryCard({ ...resultOneCountry }));
+
+        console.log(response);
+        console.log(resultOneCountry);
+      } else if (response.length <= 10) {
+        createMarkup(searchResultRef, countriesList(response));
+      } else {
+        throw 'Слишком много совпадений..';
+      }
+    })
+    .catch(error => {
+      createMarkup(searchResultRef, '');
+      pnotifyError(error);
+    });
 }
 
-function cardCountriesRender(info) {
-  const markup = countries(info.map(c => c));
-  console.log(info);
-  refs.card.insertAdjacentHTML('afterbegin', markup);
-}
+const search = () => inputRef.addEventListener('input', _debounce(inputChange, 500));
 
-// const arr = ['Hello world', 'This is a great solution', 'How are you'];
-
-// const res = {};
-
-
-
+search();
--- src/js/fetchData.js
+++
@@ -1,6 +0,0 @@
-const fetchContries = name =>
-  fetch(`https://restcountries.com/v2/name/${name}`).then(response =>
-    response.json(),
-  );
-
-export default fetchContries;
